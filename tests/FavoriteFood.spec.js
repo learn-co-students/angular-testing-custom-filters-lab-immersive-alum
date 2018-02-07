f3128eb98e30ccb178f9be5769fddc105c6609c9
sochetanie <allagorik2901@gmail.com>
2018-02-07 16:05:28 -0500

    done

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,15 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	it('should filter items by favorite food', () => {
+		const mockedArr = [
+		{ name: 'Chris', favoriteFood: 'pizza' },
+		{ name: 'Tom', favoriteFood: 'pie' },
+		{ name: 'Liz', favoriteFood: 'pie' },
+		]
+
+	const filteredArr = $filter('favoriteFood')(mockedArr, 'pie')
+	expect(filteredArr.length).toBe(2)
+	})
+
 });
